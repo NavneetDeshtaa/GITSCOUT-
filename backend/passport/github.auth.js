8c1af1dccdecee51ea72590f9e6121c674d406b3
Navneet Deshta <navneetdeshtaa@gmail.com>
2024-07-19 13:02:01 +0530

    url commit

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -18,7 +18,7 @@ passport.use(
 		{
 			clientID: process.env.GITHUB_CLIENT_ID,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET,
-			callbackURL: "/api/auth/github/callback",
+			callbackURL: "https://gitscout.onrender.com/api/auth/github/callback",
 		},
 		async function (accessToken, refreshToken, profile, done) {
 			
